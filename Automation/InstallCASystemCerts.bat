7e945c70588be7955012fb203afdc6323b9e91d4
0x0v1 <137784219+0x0v1@users.noreply.github.com>
2023-08-07 10:41:42 +0000

    Update InstallCASystemCerts.bat

--- Automation/InstallCASystemCerts.bat
+++ Automation/InstallCASystemCerts.bat
@@ -1,12 +1,12 @@
-REM This simple script supports installing both Burp Certificate and MITMProxy Certificate
-REM in as System CA certs. It assumes you have already generated hash and copied certificates
-REM for both Burp and MITMProxy. You need to modify the paths & your AVD. If you aren't using an
-REM emulator, hash out the avd section.
+::This simple script supports installing both Burp Certificate and MITMProxy Certificate
+::in as System CA certs. It assumes you have already generated hash and copied certificates
+::for both Burp and MITMProxy. You need to modify the paths & your AVD. If you aren't using an
+::emulator, hash out the avd section.
 
 @echo off
 
 echo Starting Android emulator...
-emulator -avd your_avd_name -writable-system
+start cmd /k emulator -avd Pixel_6_API_26 -writable-system
 timeout 60
 
 echo Waiting for emulator to fully start...
@@ -33,6 +33,5 @@ adb shell chmod 664 /system/etc/security/cacerts/9a5ba575.0
 adb shell chmod 664 /system/etc/security/cacerts/c8750f0d.0
 adb wait-for-device
 
-REM If you're not using an emulator, you can comment out the following line.
 echo Rebooting device...
 adb reboot
